73bd9b10f8d0972894d9a4ea9b131f434b3e471e
Hiroki Minami <hirokimnm6@gmail.com>
2023-03-03 14:23:38 -0800

    Core data

---
+++ Memo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Memo+CoreDataClass.swift
+//  RxMemo
+//
+//  Created by Hiroki Minami on 2023-03-03.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Memo)
+public class Memo: NSManagedObject {
+
+}
